9faf0eac17f4e7594119fc340c392b1f19f80da5
ryan <mr.yaryyev@gmail.com>
2026-09-14 03:35:45 +0900

    fix: refactor pages componets

--- pages/agent/index.tsx
+++ pages/agent/index.tsx
@@ -3,12 +3,14 @@ import { Stack } from "@mui/material";
 import { NextPage } from "next";
 import { useState } from "react";
 
-const Community: NextPage = () => {
+const AgentList: NextPage = () => {
+  console.log("COMMUNITY COMPONENT - PAGES ROUTER");
+  const [title, setTitle] = useState<string>("hello");
   return (
     <div style={{ margin: "20px 0" }}>
-      <Stack className="container">COMMUNITY</Stack>
+      <Stack className="container">AGENTS LIST</Stack>
     </div>
   );
 };
 
-export default withLayoutBasic(Community);
+export default withLayoutBasic(AgentList);
--- pages/property/index.tsx
+++ pages/property/index.tsx
@@ -3,11 +3,10 @@ import { Container, Stack } from "@mui/material";
 import { NextPage } from "next";
 
 const PropertyList: NextPage = () => {
-  console.log("PROPERTYLIST COMPONENT - PAGES ROUTER");
   return (
-    <>
-      <Container>PROPERTY LIST</Container>
-    </>
+    <div style={{ margin: "20px 0" }}>
+      <Stack className="container">PROPERTY LIST</Stack>
+    </div>
   );
 };
 
